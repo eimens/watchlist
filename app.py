011ea01efe529be5bb2c373a6f436cf81d13ce18
eimens <yihuakun@163.com>
2020-09-18 18:59:41 +0800

    add flask_sqlalchemy model

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
 from flask import Flask, render_template
 from flask import url_for
-
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-    ]
+from flask_sqlalchemy import SQLAlchemy
+import sys
+import os
+import click
 
 app = Flask(__name__)
 
+prefix = 'sqlite:////'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+class Cuser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    
+    user = Cuser(name=name)
+    db.session.add(user)
+
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    
+    db.session.commit()
+    click.echo('Done..')
+
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = Cuser.query.first()
+    movie = Movie.query.all()
+    return render_template('index.html', name=user.name, movies=movie)
 
     
 
